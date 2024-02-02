28530fbd6d09faa5f99c03ba6352150991e17587
Serhii Mykhailov <moderya7@gmail.com>
2024-02-02 09:22:24 +0200

    refactoring

---
+++ AccessManager.Models/Responses/GetRoomEventsCountResponse.cs
@@ -0,0 +1,14 @@
+﻿using AccessManager.Models.DataModels;
+using Newtonsoft.Json;
+
+namespace AccessManager.Models.Responses
+{
+    public class GetRoomEventsCountResponse
+    {
+        [JsonProperty("infos")]
+        public IEnumerable<RoomInfo> RoomInfos { get; set; }
+
+        [JsonProperty("cout")]
+        public int Count { get; set; }
+    }
+}
